4db6af4d67cd2bfa119c4a1a06ebedc816c3aab5
LeeMyeongHwan <lhy6693@naver.com>
2025-04-16 12:06:11 +0900

    quiz3-3.sh

--- basic3/quiz3-3.sh
+++ basic3/quiz3-3.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 
-if [ -e "file.sh" ] 
-then 
- 	if [ -s "file.sh" ] 
+if [ -e"quiz.sh" ]&&[ -s "quiz3-2.sh" ]
+then
     echo "파일이 존재하며 비어있는 파일이 아닙니다."
 else
     echo "파일이 존재하지 않거나 비어있는 파일입니다."
